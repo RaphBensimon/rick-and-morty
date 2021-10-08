69e122b03dede1d927eaee826d103f3aeb49a98f
raphael <raphael@mcledger.com>
2021-10-08 10:12:17 +0200

    Add rick and morty logo

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
 	<div id="app">
+		<header>
+			<router-link to="/">
+				<img src="@/assets/rick-and-morty.png">
+			</router-link>
+		</header>
 		<main>
 			<router-view />
 		</main>
@@ -8,14 +13,25 @@
 <style lang="scss" scoped>
 #app {
 	background: $primary;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+}
+#app > * {
+	max-width: 1200px;
+}
+header {
+	margin: 1rem 0;
+	display: flex;
+	justify-content: center;
+}
+header img {
+	width: 100%;
+	max-width: 500px;
 }
 main {
 	padding: 1rem 0;
 	max-width: 1200px;
 	width: 95%;
-	position: relative;
-	left: 50%;
-	height: 100%;
-	transform: translateX(-50%);
 }
 </style>
